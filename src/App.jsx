7f13b1d01d0a9bf4062a25376820bbc3b1492ae4
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2025-09-13 20:49:01 -0300

    diseño y responsive de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <main>
+      <main >
         <Header />
         <Presentacion />
         <Videos />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import './Navbar.css';
-import logo from "../../../Img/raices-logo-navbar.png"; // importamos la imagen del logo
+import logo from "../../../Img/raices-logo-navbar.png"; 
+import hamburg from "../../../Img/hamburg.png"; 
 
-function Navbar() {
+/* function Navbar() {
   return (
     <nav>
       <div className="logo">
@@ -15,6 +16,52 @@ function Navbar() {
       </ul>
     </nav>
   );
+} */
+
+
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
+const Navbar = () => {
+
+  const [menuEstado, setMenuEstado] = useState(false);
+
+  const cerrarMenu = () => {
+    setMenuEstado(false);
+  }
+
+  return (
+    <div className='navbar'>
+     
+
+        <div className= 'navbarContainer'>
+
+          <div className="logo">
+            <img src={logo} alt="Raíces Dúo Logo" className='logoCompleto'/>
+          </div>
+
+          {/* <FontAwesomeIcon icon={faBars} id='navbar-icon' onClick={() => setMenuEstado(!menuEstado)}/> */}
+          <img src={hamburg} alt="hamburg" id='navbar-icon' className='hamburg' onClick={() => setMenuEstado(!menuEstado)} />  
+
+          <ul className={`navlinks ${menuEstado ? 'showMenu' : ''}`}>
+            <li><a href="#videos" className="linksMenu" onClick={cerrarMenu}>Videos</a></li>
+            <li><a href="#galeria" className="linksMenu" onClick={cerrarMenu}>Galería</a></li>
+            <li><a href="#galeria" className="linksMenu" onClick={cerrarMenu}>Nosotros</a></li>
+            <li><a href="#contacto" className="linksMenu" onClick={cerrarMenu}>Contacto</a></li>
+          </ul>
+
+
+          
+        </div>
+       
+      <div style={{ height: '75px' }} />
+      
+    </div>
+  )
+
 }
+
 
 export default Navbar;
