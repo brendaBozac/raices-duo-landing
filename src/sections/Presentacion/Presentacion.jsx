19d25f0d9f098f705084f87f64513d18c2237d04
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2025-09-16 21:35:00 -0300

    estilos seccion presentacion

--- src/sections/Presentacion/Presentacion.jsx
+++ src/sections/Presentacion/Presentacion.jsx
@@ -17,17 +17,23 @@ function Presentacion() {
       {/* Row principal */}
       <div className="presentacion-row">
         <div className="col-left">
-          <h2>La Esencia del Auténtico Folklore</h2>
-          <h3>
-            Shows en vivo para Festivales, <br />
-            Bares, y Eventos Privados
-          </h3>
-          <p>
-            Somos Maryta Villalón y César Degiovanni, músicos de Río Cuarto, amantes del folklore y su sonido raíz. 
-            Trabajamos llevando nuestro Show a distintos tipos de eventos y escenarios y rendimos homenaje 
-            a poetas y cantores que dieron vida a nuestro cancionero, argentino y popular.
-          </p>
-          <Boton>Contratá el Show</Boton>
+
+          <div className='floating'>
+
+            <h2>La Esencia del Auténtico Folklore</h2>
+            <h3>
+              Shows en vivo para Festivales, <br />
+              Bares, y Eventos Privados
+            </h3>
+            <p>
+              Somos Maryta Villalón y César Degiovanni, músicos de Río Cuarto, amantes del folklore y su sonido raíz. 
+              Trabajamos llevando nuestro Show a distintos tipos de eventos y escenarios y rendimos homenaje 
+              a poetas y cantores que dieron vida a nuestro cancionero, argentino y popular.
+            </p>
+            <Boton>Contratá el Show</Boton>
+
+          </div>
+
         </div>
 
         <div className="col-right">
